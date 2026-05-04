e2e07aae9156f4d91989438e041b7c12a67efe15
marfushhaaa <p1m455w96@gmail.com>
2026-05-04 15:17:36 +0200

    feat: add Swagger and MVC Configurations

---
+++ src/main/java/ch/oberemok/marharyta/learnitself/OpenApi30Config.java
@@ -0,0 +1,23 @@
+package ch.oberemok.marharyta.learnitself;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Swagger/OpenAPI Configuration
+ * @SecurityScheme ermöglicht das Autorizieren
+ */
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "Learnitself API", version = "v1"))
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "bearer"
+)
+public class OpenApi30Config {
+
+}
---
+++ src/main/java/ch/oberemok/marharyta/learnitself/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package ch.oberemok.marharyta.learnitself;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+
+/**
+ * Konfiguriert, wie Spring URLs erkennt, vergleicht
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        PathPatternParser patternParser = new PathPatternParser();
+        patternParser.setCaseSensitive(false);
+        configurer.setPatternParser(patternParser);
+    }
+}

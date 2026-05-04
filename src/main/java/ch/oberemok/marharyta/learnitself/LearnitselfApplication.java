6dccd64441c0454b755515140c4ddc7eaf467d89
marfushhaaa <p1m455w96@gmail.com>
2026-05-04 13:45:43 +0200

    docs: initial commit

---
+++ src/main/java/ch/oberemok/marharyta/learnitself/LearnitselfApplication.java
@@ -0,0 +1,13 @@
+package ch.oberemok.marharyta.learnitself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnitselfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnitselfApplication.class, args);
+    }
+
+}

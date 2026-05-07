db81756de7c95a0284f02e67aaf2e432b5817a8a
marfushhaaa <p1m455w96@gmail.com>
2026-05-07 08:51:21 +0200

    feat: add CRUD to Kategorie

--- src/main/java/ch/oberemok/marharyta/learnitself/kategorie/KategorieController.java
+++ src/main/java/ch/oberemok/marharyta/learnitself/kategorie/KategorieController.java
@@ -1,13 +1,14 @@
 package ch.oberemok.marharyta.learnitself.kategorie;
 
+import ch.oberemok.marharyta.learnitself.base.MessageResponse;
 import ch.oberemok.marharyta.learnitself.security.Roles;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.annotation.security.RolesAllowed;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,10 +22,35 @@ public class KategorieController {
         this.kategorieService = kategorieService;
     }
 
+    @GetMapping("api/kategorie/{id}")
+    @RolesAllowed(Roles.Read)
+    public ResponseEntity<Kategorie> one(@PathVariable Long id) {
+        Kategorie result = kategorieService.getKategorie(id);
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
     @GetMapping("api/kategorie")
     @RolesAllowed(Roles.Read)
     public ResponseEntity<List<Kategorie>> all() {
         List<Kategorie> result = kategorieService.getKategorien();
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PostMapping("api/kategorie")
+    @RolesAllowed(Roles.Admin)
+    public ResponseEntity<Kategorie> newKategorie(@Valid @RequestBody Kategorie kategorie) {
+        Kategorie newKategorie = kategorieService.insertKategorie(kategorie);
+        return new ResponseEntity<>(newKategorie, HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("api/kategorie/{id}")
+    @RolesAllowed(Roles.Admin)
+    public ResponseEntity<MessageResponse> deleteKategorie(@PathVariable Long id) {
+        try {
+            return ResponseEntity.ok(kategorieService.deleteKategorie(id));
+        } catch (Throwable t) {
+            return ResponseEntity.internalServerError().build();
+        }
     }
 }
